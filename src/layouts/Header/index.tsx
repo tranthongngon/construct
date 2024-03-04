a8073e9776981a1c1d275cd46edac3461264d6c3
tranthongngon <tranngon98@gmail.com>
2024-03-04 18:54:25 +0700

    add style for header

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -3,26 +3,52 @@ import { useTheme } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/images/logo.png";
+import { styled } from "@mui/material/styles";
+import "./style.scss";
 
 export default function Header() {
   const theme = useTheme();
+  const Header = styled("header")(({ theme }) => ({
+    background: theme.palette.primary.dark,
+    padding: "10px 0",
+    color: theme.palette.common.white,
+    ".header__wrap": {
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: 'center',
+      ".header__logo": {
+        width: "100px",
+      },
+      ".header__left": {
+        '&-left': {
+          background: 'transparent',
+          width: '30px',
+          height: '30px',
+          border: 'none',
+          outline: 'none'
+        }
+      }
+    },
+  }));
   return (
-    <header className="header">
+    <Header className="header">
       <div className="container">
-        <div className="header__left">
-          <button className="header__left-btn">
-            <MenuIcon color="primary" />
-          </button>
-        </div>
-        <div className="header__logo">
-          <Link to="/">
-            <img src={Logo} alt="construct" />
-          </Link>
-        </div>
-        <div className="header__languages">
-          
+        <div className="header__wrap">
+          <div className="header__left">
+            <button className="header__left-btn">
+              <MenuIcon color="primary" />
+            </button>
+          </div>
+          <div className="header__logo">
+            <Link to="/">
+              <img src={Logo} alt="construct" />
+            </Link>
+          </div>
+          <div className="header__languages">
+            <button>VN</button>|<button>EN</button>
+          </div>
         </div>
       </div>
-    </header>
+    </Header>
   );
 }

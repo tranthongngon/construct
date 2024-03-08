8f5fed001e6fba861640e8633a9c7df6509d053d
Ngôn <56878771+tranthongngon@users.noreply.github.com>
2024-03-08 09:42:07 +0700

    update image

--- src/components/BannerSilde/data.tsx
+++ src/components/BannerSilde/data.tsx
@@ -4,7 +4,7 @@ import { loadImage } from "../../utils";
 export const staticData: ISlide[] = [
   {
     title: "There are many variations of passages of Lorem Ipsum.",
-    url: loadImage("slide_1.png"),
+    url: loadImage("slide_1.jpeg"),
     text: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
   },
   {

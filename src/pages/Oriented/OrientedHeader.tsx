7715ae35c96f422e4a6f55b6593464e4ba53698f
TrongVy <83958160+TrongVy@users.noreply.github.com>
2024-03-12 11:01:56 +0700

    Update OrientedHeader.tsx

--- src/pages/Oriented/OrientedHeader.tsx
+++ src/pages/Oriented/OrientedHeader.tsx
@@ -1,23 +1,43 @@
 import { Typography, styled } from '@mui/material';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import { useSpring, animated } from '@react-spring/web'
+
 const Content = styled('div')({
-  borderBottom: '1px solid black',
-  height: '130px',
-  color: '#16315e',
-  display: 'flex',
-  justifyContent: 'space-between',
-  h2: {
-    fontFamily: 'cursive !important',
-  }
+    height: '130px',
+    color: '#16315e',
+    display: 'flex',
+    justifyContent: 'space-between',
+    h2: {
+        fontFamily: 'cursive !important',
+    }
 });
 
 export default function OrientedHeader() {
 
-  return (
-    <Content className="oriented__header">
-      <Typography variant="h2" > ĐỊNH HƯỚNG <br /> PHÁT TRIỂN </Typography>
-      <ArrowUpwardIcon style={{ fontSize: '40px', color: '#16315e',marginTop: '20px' }}/>
-    </Content>
-  )
+    const springs = useSpring({
+        from: { x: -1500 },
+        to: { x: 0 },
+    })
+
+    return (
+        <>
+            <Content className="oriented__header">
+                <Typography variant="h2" > ĐỊNH HƯỚNG <br /> PHÁT TRIỂN </Typography>
+                <ArrowUpwardIcon style={{ fontSize: '40px', color: '#16315e', marginTop: '20px' }} />
+            </Content>
+
+            <animated.div
+                style={{
+                    width: "100%",
+                    height: "1px",
+                    background: '#000',
+                    borderRadius: 1,
+                    ...springs,
+                }}
+            />
+
+        </>
+
+    )
 
 }

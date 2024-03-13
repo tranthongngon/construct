60fb11a13df20579a6c877891f994551d050b2aa
TrongVy <83958160+TrongVy@users.noreply.github.com>
2024-03-13 14:57:43 +0700

    update

--- src/pages/Oriented/Diversification.tsx
+++ src/pages/Oriented/Diversification.tsx
@@ -1,6 +1,47 @@
+import { Grid, Typography } from "@mui/material";
+import { useSpring, animated } from '@react-spring/web';
+import Image1 from '../../assets/images/Diversification1.jpg';
+import Image2 from '../../assets/images/Diversification2.jpg';
 
 export default function Diversification() {
+  const springs = useSpring({
+    from: { x: -1500 },
+    to: { x: 0 },
+  })
   return (
-    <div>Diversification</div>
+    <>
+          <Grid container spacing={2} sx={{ mt: 2 }}>
+        <Grid item xs={12} md={12} style={{}}>
+          <Typography variant="h3" style={{ color: "#000", fontWeight: 400 }}>Đa dạng văn hóa</Typography>
+        </Grid>
+
+        <animated.div
+          style={{
+            width: "100%",
+            height: "1px",
+            background: '#000',
+            borderRadius: 1,
+            ...springs,
+          }}
+        />
+        <Grid item xs={12} md={6} style={{}}>
+          <Typography variant="h6" style={{fontStyle:"italic"}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam similique reiciendis illum voluptatibus voluptatum necessitatibus, deserunt reiciendis dolore obcaecati veritatis quasi nu illum voluptatibus </Typography>
+        </Grid>
+      </Grid>
+
+
+      <Grid container spacing={2} sx={{ mt: 2 }}>
+        <Grid item xs={12} md={6} style={{}} sx={{ p: 4 }}>
+          <div>
+            <img src={Image1} alt="" style={{ width: "100%", height: "500px" }} />
+          </div>
+        </Grid>
+        <Grid item xs={12} md={6} style={{}} sx={{ p: 4 }}>
+          <div>
+            <img src={Image2} alt="" style={{ width: "100%", height: "500px" }} />
+          </div>
+        </Grid>
+      </Grid>
+    </>
   )
 }

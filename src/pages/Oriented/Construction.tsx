af7624f7e5ee7fb139940de1763123478f43eaab
TrongVy <83958160+TrongVy@users.noreply.github.com>
2024-03-13 10:52:35 +0700

    update

--- src/pages/Oriented/Construction.tsx
+++ src/pages/Oriented/Construction.tsx
@@ -79,12 +79,12 @@ export default function Construction() {
                         <Typography style={{ fontFamily: 'cursive', color: 'red' }} variant="h4">TỔNG THẦU</Typography>
                         <p style={{ fontStyle: 'italic' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora veritatis vitae, tempore ducimus earum magnam ex ullam amet sequi beatae consequuntur ipsa soluta similique cupiditate nemo. Accusamus quasi placeat magni tempora porro ullam. Eligendi, assumenda atque fugit laboriosam culpa minus tenetur dolor sint explicabo eius possimus eum qui deserunt exercitationem! Placeat soluta perspiciatis nisi veritatis necessitatibus aperiam officia pariatur! Quasi aut quidem sit officiis eos esse temporibus, mollitia maxime odit ipsum et nulla, illum porro dolor molestiae at eius itaque corrupti reprehenderit illo? Rem nihil voluptas veritatis quod dicta pariatur facere nobis. Possimus labore, ab quibusdam laborum quam obcaecati nobis.</p>
                     </div>
-                    <div style={{border:'2px solid red',padding:'2px'}}>
+                    <div style={{border:'2px solid red',padding:'2px',boxShadow: '5px 10px'}} className='tongThau-img'>
                         <img src={Image3} alt="" style={{ width: "100%", height: "500px" }} />
                     </div>
                 </Grid>
 
-                <Grid item xs={12} md={4} sx={{ p: 4 }} style={{ marginTop: "112px" }}>
+                <Grid item xs={12} md={4} sx={{ p: 4 }} style={{ marginTop: "112px" }} className="DesignAndBuild">
                     <animated.div
                         style={{
                             width: "100%",
@@ -98,12 +98,12 @@ export default function Construction() {
                         <Typography style={{ fontFamily: 'cursive', color: 'blue' }} variant="h4">DESIGN & BUILD</Typography>
                         <p style={{ fontStyle: 'italic' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora veritatis vitae, tempore ducimus earum magnam ex ullam amet sequi beatae consequuntur ipsa soluta similique cupiditate nemo. Accusamus quasi placeat magni tempora porro ullam. Eligendi, assumenda atque fugit laboriosam culpa minus tenetur dolor sint explicabo eius possimus eum qui deserunt exercitationem! Placeat soluta perspiciatis nisi veritatis necessitatibus aperiam officia pariatur! Quasi aut quidem sit officiis eos esse temporibus, mollitia maxime odit ipsum et nulla, illum porro dolor molestiae at eius itaque corrupti reprehenderit illo? Rem nihil voluptas veritatis quod dicta pariatur facere nobis. Possimus labore, ab quibusdam laborum quam obcaecati nobis.</p>
                     </div>
-                    <div style={{border:'2px solid blue',padding:'2px'}}>
+                    <div style={{border:'2px solid blue',padding:'2px',boxShadow: '5px 10px'}} className='DesignAndBuild-img styleImg'>
                         <img src={Image4} alt="" style={{ width: "100%", height: "550px" }} />
                     </div>
                 </Grid>
 
-                <Grid item xs={12} md={4} sx={{ p: 4 }} style={{ marginTop: "222px" }}>
+                <Grid item xs={12} md={4} sx={{ p: 4 }} style={{ marginTop: "222px" }} className='MAndE' >
                     <animated.div
                         style={{
                             width: "100%",
@@ -117,7 +117,7 @@ export default function Construction() {
                         <Typography style={{ fontFamily: 'cursive', color: '#969608' }} variant="h4">M&E</Typography>
                         <p style={{ fontStyle: 'italic' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora veritatis vitae, tempore ducimus earum magnam ex ullam amet sequi beatae consequuntur ipsa soluta similique cupiditate nemo. Accusamus quasi placeat magni tempora porro ullam. Eligendi, assumenda atque fugit laboriosam culpa minus tenetur dolor sint explicabo eius possimus eum qui deserunt exercitationem! Placeat soluta perspiciatis nisi veritatis necessitatibus aperiam officia pariatur! Quasi aut quidem sit officiis eos esse temporibus, mollitia maxime odit ipsum et nulla, illum porro dolor molestiae at eius itaque corrupti reprehenderit illo? Rem nihil voluptas veritatis quod dicta pariatur facere nobis. Possimus labore, ab quibusdam laborum quam obcaecati nobis.</p>
                     </div>
-                    <div style={{border:'2px solid #969608',padding:'2px'}}>
+                    <div style={{border:'2px solid #969608',padding:'2px',boxShadow: '5px 10px '}} className="MAndE-img">
                         <img src={Image5} alt="" style={{ width: "100%", height: "600px" }} />
                     </div>
                 </Grid> 
